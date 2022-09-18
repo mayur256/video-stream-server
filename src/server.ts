c1823a692697cacb5fde2e51f1b415c64751b71a
Mayur Upadhayay <mayur123a@gmail.com>
2022-09-18 11:51:55 +0530

    feat: Completed initial implemention of video streaming

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 // HTTP module to create a server
 import http, { IncomingMessage, ServerResponse } from "node:http";
 import fs from "fs/promises";
+import { statSync, createReadStream } from "node:fs";
 
 const PORT = 8001;
 // create a local server to receive data from
@@ -17,7 +18,31 @@ const server: any = http.createServer((request: IncomingMessage, response: Serve
             }).end(headerAbsentError)
         }
 
-        
+        // Get video file size
+        const videoPath = "index.mp4";
+        const videoSize = statSync(videoPath).size;
+
+        // calculate chunk size to be sent to the client - here 1 MB
+        const CHUNK_SIZE = 10 ** 6;
+        const start = Number(range?.replace(/\D/g, ""));
+        const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+
+        // response headers for video
+        const vHeaders = {
+            "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+            "Accept-Ranges": "bytes",
+            "Content-Length": end - start + 1,
+            "Content-Type": "video/mp4",
+        }
+
+        // set the headers for chunk data
+        response.writeHead(206, vHeaders);
+
+        // create a stream to get chunk of video
+        const videoStream = createReadStream(videoPath, { start, end });
+
+        // pipe the chunk into the output stream
+        videoStream.pipe(response);
     } else {
         // send the index file
         (async(response) => {
